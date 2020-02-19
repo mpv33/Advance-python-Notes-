f733916d4f376ccd4524caee95f8eabf8e2ba09a
mateshwari verma <46149432+mpv33@users.noreply.github.com>
2020-02-19 13:44:00 +0530

    Add files via upload

---
+++ Threads/Threading.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python
+import threading
+import time
+exitFlag = 0
+class myThread (threading.Thread):
+   def __init__(self, threadID, name, counter):
+      threading.Thread.__init__(self)
+      self.threadID = threadID
+      self.name = name
+      self.counter = counter
+   def run(self):
+      print("Starting " + self.name)
+      print_time(self.name, 5, self.counter)
+      print("Exiting " + self.name)
+def print_time(threadName, counter, delay):
+   while counter:
+      if exitFlag:
+         threadName.exit()
+      time.sleep(delay)
+      print("%s: %s" % (threadName, time.ctime(time.time())))
+      counter -= 1
+# Create new threads
+thread1 = myThread(1, "Thread-1", 1)
+thread2 = myThread(2, "Thread-2", 2)
+# Start new Threads
+thread1.start()
+thread2.start()
+print("Exiting Main Thread")
+
+'''
+When the above code is executed, it produces the following result −
+Starting Thread-1
+Starting Thread-2
+Exiting Main Thread
+Thread-1: Thu Mar 21 09:10:03 2013
+Thread-1: Thu Mar 21 09:10:04 2013
+Thread-2: Thu Mar 21 09:10:04 2013
+Thread-1: Thu Mar 21 09:10:05 2013
+Thread-1: Thu Mar 21 09:10:06 2013
+Thread-2: Thu Mar 21 09:10:06 2013
+Thread-1: Thu Mar 21 09:10:07 2013
+Exiting Thread-1
+Thread-2: Thu Mar 21 09:10:08 2013
+Thread-2: Thu Mar 21 09:10:10 2013
+Thread-2: Thu Mar 21 09:10:12 2013
+Exiting Thread-2
+'''
